8c0eaaf6fdf6612e506c5f0872a8e3103b980ab8
akashahmed-coder <86545072+akashahmed-coder@users.noreply.github.com>
2022-07-04 12:26:28 +0600

    dadaadfsdf

--- server.js
+++ server.js
@@ -48,7 +48,9 @@ const PORT = process.env.PORT || 5000
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
-    app.use(path(__dirname,"client","build","index.html"))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,'client','build','index.html'))
+    })
 }
 
 
